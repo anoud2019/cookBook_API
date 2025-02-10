03b284787ba0b931c1e44dd59796d82c8a896e2b
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2025-02-10 09:45:33 +0400

    Added space after comma

--- src/main/java/com/cookBook/cookbook_api/Repositories/RecipeRepository.java
+++ src/main/java/com/cookBook/cookbook_api/Repositories/RecipeRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface RecipeRepository extends JpaRepository<Recipe,Integer> {
+public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
     List<Recipe> findByIngredientsNameIn(List<String> ingredientNames);
 }
