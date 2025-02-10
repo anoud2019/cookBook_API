bd3fd9a1cc60d6e479da32212f470060a6369a04
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2025-02-10 09:44:48 +0400

    Added check if null or not in methods

--- src/main/java/com/cookBook/cookbook_api/DTOS/RecipeDTO.java
+++ src/main/java/com/cookBook/cookbook_api/DTOS/RecipeDTO.java
@@ -2,6 +2,7 @@ package com.cookBook.cookbook_api.DTOS;
 
 import com.cookBook.cookbook_api.Models.Ingredient;
 import com.cookBook.cookbook_api.Models.Recipe;
+import com.cookBook.cookbook_api.Utils.HelperUtils;
 
 
 import java.util.ArrayList;
@@ -59,50 +60,62 @@ public class RecipeDTO {
 
     public static RecipeDTO convertToDTO(Recipe recipe) {
         RecipeDTO recipeDTO = new RecipeDTO();
-        recipeDTO.setId(recipe.getId());
-        recipeDTO.setName(recipe.getName());
-        recipeDTO.setInstructions(recipe.getInstructions());
+        if (HelperUtils.isNotNull(recipe)) {
+            recipeDTO.setId(recipe.getId());
+            recipeDTO.setName(recipe.getName());
+            recipeDTO.setInstructions(recipe.getInstructions());
 
-        List<String> ingredientNames = new ArrayList<>();
-        for (Ingredient ingredient : recipe.getIngredients()) {
-            ingredientNames.add(ingredient.getName());
+            List<String> ingredientNames = new ArrayList<>();
+            if (HelperUtils.isNotNull(recipe.getIngredients())) {
+                for (Ingredient ingredient : recipe.getIngredients()) {
+                    ingredientNames.add(ingredient.getName());
+                }
+            }
+            recipeDTO.setIngredients(ingredientNames);
         }
-        recipeDTO.setIngredients(ingredientNames);
 
         return recipeDTO;
     }
 
     public static List<RecipeDTO> convertToDTO(List<Recipe> recipeList) {
         List<RecipeDTO> recipeDTOList = new ArrayList<>();
-        for (Recipe recipe : recipeList) {
-            recipeDTOList.add(convertToDTO(recipe));
+        if (!recipeDTOList.isEmpty()) {
+            for (Recipe recipe : recipeList) {
+                recipeDTOList.add(convertToDTO(recipe));
+            }
         }
         return recipeDTOList;
     }
 
     public static Recipe convertFromDTO(RecipeDTO recipeDTO) {
         Recipe recipe = new Recipe();
-        recipe.setId(recipeDTO.getId());
-        recipe.setName(recipeDTO.getName());
-        recipe.setInstructions(recipeDTO.getInstructions());
+        if (HelperUtils.isNotNull(recipeDTO)) {
+            recipe.setId(recipeDTO.getId());
+            recipe.setName(recipeDTO.getName());
+            recipe.setInstructions(recipeDTO.getInstructions());
 
 
-        List<Ingredient> ingredients = new ArrayList<>();
-        for (String ingredientName : recipeDTO.getIngredients()) {
-            Ingredient ingredient = new Ingredient();
-            ingredient.setName(ingredientName);
-            ingredients.add(ingredient);
+            List<Ingredient> ingredients = new ArrayList<>();
+            if (HelperUtils.isNotNull(recipeDTO.getIngredients())) {
+                for (String ingredientName : recipeDTO.getIngredients()) {
+                    Ingredient ingredient = new Ingredient();
+                    ingredient.setName(ingredientName);
+                    ingredients.add(ingredient);
+                }
+            }
+            recipe.setIngredients(ingredients);
+
+            return recipe;
         }
-        recipe.setIngredients(ingredients);
-
-        return recipe;
     }
 
     public static List<Recipe> convertFromDTO(List<RecipeDTO> recipeDTOList) {
         List<Recipe> recipeList = new ArrayList<>();
-        for (RecipeDTO recipeDTO : recipeDTOList) {
-            Recipe recipe = convertFromDTO(recipeDTO);
-            recipeList.add(recipe);
+        if (!recipeList.isEmpty()) {
+            for (RecipeDTO recipeDTO : recipeDTOList) {
+                // Recipe recipe = convertFromDTO(recipeDTO);
+                recipeList.add(RecipeDTO.convertFromDTO(recipeDTO));
+            }
         }
         return recipeList;
     }
