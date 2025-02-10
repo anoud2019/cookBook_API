93c961495da10b0b6c5b728d41a92a37ed6efa5c
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2025-02-10 09:39:38 +0400

    Added HelperUtils class

--- src/main/java/com/cookBook/cookbook_api/Utils/HelperUtils.java
+++ src/main/java/com/cookBook/cookbook_api/Utils/HelperUtils.java
@@ -1,4 +1,20 @@
 package com.cookBook.cookbook_api.Utils;
 
+import java.util.List;
+
 public class HelperUtils {
+    public static <F> boolean isNull(F value) {
+        return value == null;
+    }
+
+    public static <A> boolean isNotNull(A value) {
+        return value != null;
+    }
+    public static boolean isNotEmpty(String value) {
+        return value != null && !value.trim().isEmpty();
+    }
+    public static <T> boolean isNotEmpty(List<T> list) {
+        return list != null && !list.isEmpty();
+    }
+
 }
