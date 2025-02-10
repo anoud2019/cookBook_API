5117dc0e1a5b4a634b09ba537e736b9b05c203bd
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2025-02-10 09:36:16 +0400

    Added Get method

---
+++ src/main/java/com/cookBook/cookbook_api/Controllers/RecipeController.java
@@ -0,0 +1,31 @@
+package com.cookBook.cookbook_api.Controllers;
+
+import com.cookBook.cookbook_api.DTOS.RecipeDTO;
+import com.cookBook.cookbook_api.Services.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "recipes")
+public class RecipeController {
+
+    @Autowired
+    private RecipeService recipeService;
+
+    @GetMapping(value = "/getAll")
+    public List<RecipeDTO> getAllRecipes() {
+        List<RecipeDTO> recipeDTOList = new ArrayList<>();
+        try {
+            recipeDTOList.addAll(recipeService.getAllRecipes());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+        }
+        return recipeDTOList;
+    }
+
+
+}
