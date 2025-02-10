51dea551e3e9ba38f3e65056df214225d4894662
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2025-02-10 13:51:57 +0400

    Added IngredientController

---
+++ src/main/java/com/cookBook/cookbook_api/Controllers/IngredientController.java
@@ -0,0 +1,31 @@
+package com.cookBook.cookbook_api.Controllers;
+
+import com.cookBook.cookbook_api.DTOS.IngredientDTO;
+import com.cookBook.cookbook_api.DTOS.RecipeDTO;
+import com.cookBook.cookbook_api.Services.IngredientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ingredients")
+public class IngredientController {
+    @Autowired
+    private IngredientService ingredientService;
+
+    @PostMapping(value = "/addIngredients")
+    public IngredientDTO add(@RequestBody IngredientDTO dto) {
+        IngredientDTO ingredientDTO = new IngredientDTO();
+        try {
+            ingredientDTO = ingredientService.addIngredient(dto);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+        }
+        return ingredientDTO;
+    }
+
+
+}
