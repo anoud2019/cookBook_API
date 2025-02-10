0bef45496400b47c73fcd34eeda12501c9f5ee4f
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2025-02-10 13:51:07 +0400

    update in add methode

--- src/main/java/com/cookBook/cookbook_api/Services/IngredientService.java
+++ src/main/java/com/cookBook/cookbook_api/Services/IngredientService.java
@@ -2,24 +2,42 @@ package com.cookBook.cookbook_api.Services;
 
 import com.cookBook.cookbook_api.DTOS.IngredientDTO;
 import com.cookBook.cookbook_api.Models.Ingredient;
+import com.cookBook.cookbook_api.Models.Recipe;
 import com.cookBook.cookbook_api.Repositories.IngredientRepository;
+import com.cookBook.cookbook_api.Repositories.RecipeRepository;
+import com.cookBook.cookbook_api.Utils.HelperUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class IngredientService {
     @Autowired
     private IngredientRepository ingredientRepository;
+    @Autowired
+    private RecipeRepository recipeRepository;
 
     public List<IngredientDTO> getAllIngredients() {
         List<Ingredient> ingredients = ingredientRepository.findAll();
         return IngredientDTO.convertToDTO(ingredients);
     }
-    public IngredientDTO addIngredient(IngredientDTO ingredientDTO){
-        Ingredient ingredient=IngredientDTO.convertFromDTO(ingredientDTO);
-        Ingredient saveIngredient=ingredientRepository.save(ingredient);
+
+    public IngredientDTO addIngredient(IngredientDTO ingredientDTO) {
+        Ingredient ingredient = IngredientDTO.convertFromDTO(ingredientDTO);
+        List<Recipe> recipes = new ArrayList<>();
+        for (String recipeId : ingredientDTO.getRecipes()) {
+            Integer id = Integer.parseInt(recipeId);
+            Recipe recipe = recipeRepository.findById(id).orElse(null);
+            if (HelperUtils.isNotNull(recipe)) {
+                recipes.add(recipe);
+            }
+
+        }
+        ingredient.setRecipes(recipes);
+        Ingredient saveIngredient = ingredientRepository.save(ingredient);
         return IngredientDTO.convertToDTO(saveIngredient);
 
     }
