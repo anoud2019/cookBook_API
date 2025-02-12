e438749047c739a5283a7de7fc1a68853a0ade45
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2025-02-12 08:34:03 +0400

    Added findByName method

--- src/main/java/com/cookBook/cookbook_api/Repositories/IngredientRepository.java
+++ src/main/java/com/cookBook/cookbook_api/Repositories/IngredientRepository.java
@@ -4,5 +4,5 @@ import com.cookBook.cookbook_api.Models.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IngredientRepository  extends JpaRepository<Ingredient,Integer> {
-
+    Ingredient findByName(String name);
 }
