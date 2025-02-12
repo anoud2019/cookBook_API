9a160ede327e73a1ceb7648dbb6edbb7bedb5284
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2025-02-12 08:32:52 +0400

    Added Delete method

---
+++ src/main/java/com/cookBook/cookbook_api/RequestObject/AddIngredientRequest.java
@@ -0,0 +1,32 @@
+package com.cookBook.cookbook_api.RequestObject;
+
+import java.util.List;
+
+public class AddIngredientRequest {
+    private Integer ingredientId;
+    private String ingredientName;
+    private List<Integer> recipeIds;
+
+    public Integer getIngredientId() {
+        return ingredientId;
+    }
+
+    public void setIngredientId(Integer ingredientId) {
+        this.ingredientId = ingredientId;
+    }
+    public String getIngredientName() {
+        return ingredientName;
+    }
+
+    public void setIngredientName(String ingredientName) {
+        this.ingredientName = ingredientName;
+    }
+
+    public List<Integer> getRecipeIds() {
+        return recipeIds;
+    }
+
+    public void setRecipeIds(List<Integer> recipeIds) {
+        this.recipeIds = recipeIds;
+    }
+}
