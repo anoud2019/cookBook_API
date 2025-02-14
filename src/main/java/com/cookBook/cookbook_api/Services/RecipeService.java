d6557d797b7c1411ca78d19626a6a4da95293ff6
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2025-02-14 22:25:32 +0400

    added @Autowired

--- src/main/java/com/cookBook/cookbook_api/Services/RecipeService.java
+++ src/main/java/com/cookBook/cookbook_api/Services/RecipeService.java
@@ -1,12 +1,16 @@
 package com.cookBook.cookbook_api.Services;
 
+import com.cookBook.cookbook_api.DTOS.IngredientDTO;
 import com.cookBook.cookbook_api.DTOS.RecipeDTO;
+import com.cookBook.cookbook_api.Models.Ingredient;
 import com.cookBook.cookbook_api.Models.Recipe;
+import com.cookBook.cookbook_api.Repositories.IngredientRepository;
 import com.cookBook.cookbook_api.Repositories.RecipeRepository;
 import com.cookBook.cookbook_api.Utils.HelperUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +18,12 @@ import java.util.Optional;
 public class RecipeService {
     @Autowired
     private RecipeRepository recipeRepository;
+
+    @Autowired
+    private IngredientRepository ingredientRepository;
+
+    @Autowired
+    IngredientService ingredientService;
 
     public List<RecipeDTO> getAllRecipes() {
         List<Recipe> recipes = recipeRepository.findAll();
