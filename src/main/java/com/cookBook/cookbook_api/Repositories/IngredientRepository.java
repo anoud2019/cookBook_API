c2864dc641536253e1f9b667487dd1eed81ec46a
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2025-02-14 22:20:30 +0400

    added query

--- src/main/java/com/cookBook/cookbook_api/Repositories/IngredientRepository.java
+++ src/main/java/com/cookBook/cookbook_api/Repositories/IngredientRepository.java
@@ -1,8 +1,15 @@
 package com.cookBook.cookbook_api.Repositories;
 
+
 import com.cookBook.cookbook_api.Models.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface IngredientRepository  extends JpaRepository<Ingredient,Integer> {
-    Ingredient findByName(String name);
+import java.util.List;
+
+public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
+    @Query("SELECT i FROM Ingredient i WHERE i.id = :id")
+    Ingredient getIngredientById(@Param("id") Integer id);
+
 }
