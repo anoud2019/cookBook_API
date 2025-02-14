3b27b57f2383c68034928467dae5be85b5ce073d
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2025-02-14 22:03:35 +0400

    Update IngredientController.java

--- src/main/java/com/cookBook/cookbook_api/Controllers/IngredientController.java
+++ src/main/java/com/cookBook/cookbook_api/Controllers/IngredientController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ingredients")
