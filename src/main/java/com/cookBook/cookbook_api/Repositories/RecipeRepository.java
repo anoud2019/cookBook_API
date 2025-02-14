8e28420ff903f54260042c2be6737c4dc4531287
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2025-02-14 22:21:04 +0400

    added query

--- src/main/java/com/cookBook/cookbook_api/Repositories/RecipeRepository.java
+++ src/main/java/com/cookBook/cookbook_api/Repositories/RecipeRepository.java
@@ -1,10 +1,22 @@
 package com.cookBook.cookbook_api.Repositories;
 
+import com.cookBook.cookbook_api.Models.Ingredient;
 import com.cookBook.cookbook_api.Models.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
-    List<Recipe> findByIngredientsNameIn(List<String> ingredientNames);
+    @Query("SELECT r FROM Recipe r WHERE r.id = :id")
+    Recipe getRecipeById(@Param("id") Integer id);
+
+    @Query("SELECT r FROM Recipe r JOIN r.ingredient i WHERE i.name IN :ingredients")
+    List<Recipe> findRecipesByIngredients(@Param("ingredients") List<String> ingredients);
+
+    @Query("SELECT r FROM Recipe r WHERE r.name LIKE %:name%")
+    List<Recipe> findRecipesByNameContaining(@Param("name") String name);
+
+
 }
