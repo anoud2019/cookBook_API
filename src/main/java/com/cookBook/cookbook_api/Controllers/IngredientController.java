b070008d3d62bb41364fb7fd050750f28bb11b2a
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2025-02-14 21:56:04 +0400

    Added import Logger

--- src/main/java/com/cookBook/cookbook_api/Controllers/IngredientController.java
+++ src/main/java/com/cookBook/cookbook_api/Controllers/IngredientController.java
@@ -3,6 +3,8 @@ package com.cookBook.cookbook_api.Controllers;
 import com.cookBook.cookbook_api.DTOS.IngredientDTO;
 import com.cookBook.cookbook_api.DTOS.RecipeDTO;
 import com.cookBook.cookbook_api.Services.IngredientService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/ingredients")
 public class IngredientController {
+    private final Logger logger = LoggerFactory.getLogger(IngredientController.class);
+
     @Autowired
     private IngredientService ingredientService;
 
