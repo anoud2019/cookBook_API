de6b072d96a4e5a811489f14da36b5d663078dd0
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2025-02-17 14:06:42 +0400

    delete list method

--- src/main/java/com/cookBook/cookbook_api/Services/RecipeService.java
+++ src/main/java/com/cookBook/cookbook_api/Services/RecipeService.java
@@ -156,10 +156,7 @@ public class RecipeService {
         return false;
     }
 
-    public List<RecipeDTO> searchRecipesByIngredients(List<String> ingredients) {
-        List<Recipe> recipes = recipeRepository.findRecipesByIngredients(ingredients);
 
-        List<RecipeDTO> recipeDTOList = new ArrayList<>();
         for (Recipe recipe : recipes) {
             recipeDTOList.add(RecipeDTO.convertToDTO(recipe));
         }
