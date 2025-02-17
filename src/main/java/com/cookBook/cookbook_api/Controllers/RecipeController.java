6bbf3f0a8dd1c9e88140ff7d18b64ed74db12c2c
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2025-02-17 13:58:22 +0400

    use set

--- src/main/java/com/cookBook/cookbook_api/Controllers/RecipeController.java
+++ src/main/java/com/cookBook/cookbook_api/Controllers/RecipeController.java
@@ -8,6 +8,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
+
+
+import java.util.Set;
 
 @RestController
 @RequestMapping(value = "recipes")
