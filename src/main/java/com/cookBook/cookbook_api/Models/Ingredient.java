1217c7c9df371e7fc3f7881b74fc6f64693687fa
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2025-02-17 14:19:51 +0400

    delete list

--- src/main/java/com/cookBook/cookbook_api/Models/Ingredient.java
+++ src/main/java/com/cookBook/cookbook_api/Models/Ingredient.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 
-import java.util.List;
 
 @Entity
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
