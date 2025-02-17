4ed551235be633ce1ed5cfbc11f3aa56d1d76372
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2025-02-17 14:09:08 +0400

    use set

--- src/main/java/com/cookBook/cookbook_api/Services/IngredientService.java
+++ src/main/java/com/cookBook/cookbook_api/Services/IngredientService.java
@@ -11,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 
 @Service
