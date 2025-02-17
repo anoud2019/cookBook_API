599ebbf180ddd084ed8443e26d9ca7cd4d6a43f9
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2025-02-17 14:17:38 +0400

    delete list

--- src/main/java/com/cookBook/cookbook_api/Repositories/RecipeRepository.java
+++ src/main/java/com/cookBook/cookbook_api/Repositories/RecipeRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
     @Query("SELECT r FROM Recipe r WHERE r.id = :id")
