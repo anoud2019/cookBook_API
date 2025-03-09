f8bab24f062ee181f39d254cf9e43da02b7d735f
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2025-03-09 09:12:55 +0400

    Create UserService.java

---
+++ src/main/java/com/cookBook/cookbook_api/Services/UserService.java
@@ -0,0 +1,30 @@
+package com.cookBook.cookbook_api.Services;
+
+import com.cookBook.cookbook_api.Models.User;
+import com.cookBook.cookbook_api.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUserName())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
