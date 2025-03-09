f28117aa0c71f9cd38a0e64ad48d4534926b3a06
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2025-03-09 09:10:33 +0400

    Create User.java

---
+++ src/main/java/com/cookBook/cookbook_api/Models/User.java
@@ -0,0 +1,127 @@
+package com.cookBook.cookbook_api.Models;
+
+import com.cookBook.cookbook_api.Enums.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    boolean isActive;
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer id;
+
+    @NotBlank(message = "Username is needed")
+    @Column(unique = true, nullable = false)
+    public String userName;
+
+    @NotBlank(message = "Password is needed")
+    @Column(nullable = false)
+    public String password;
+
+    @NotBlank(message = "Email is needed")
+    @Column(unique = true, nullable = false)
+    public String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    public Role role;
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public User() {
+        this.isActive = true;
+    }
+
+    public User(String email, String password, Role role, String firstName) {
+        this.userName = firstName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
