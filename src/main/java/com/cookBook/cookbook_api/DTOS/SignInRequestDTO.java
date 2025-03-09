06712f4e94c243cb9e12d00bee3b0ecf139b33f7
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2025-03-09 09:08:51 +0400

    Added SignInRequestDTO class

---
+++ src/main/java/com/cookBook/cookbook_api/DTOS/SignInRequestDTO.java
@@ -0,0 +1,26 @@
+package com.cookBook.cookbook_api.DTOS;
+
+import lombok.Data;
+
+@Data
+public class SignInRequestDTO {
+
+    String userName;
+    String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
